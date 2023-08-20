d7dd07c0354c2556de73c19f075b3c3f6daa6dd5
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-08-20 23:45:14 +0100

    feat: disabled serializableCheck in redux middleware

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookmarkReducer from "./features/bookmarkSlice";
 
-
 export const store = configureStore({
   reducer: {
-    bookmark: bookmarkReducer, 
+    bookmark: bookmarkReducer,
   },
-  devTools: process.env.NODE_ENV !== "production",
+ middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+      devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>;
