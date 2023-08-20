91e3b9193f02755bdbf9cb3138cdb4d37fc75e20
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-08-20 23:48:13 +0100

    feat: added google profile image for signed in accounts

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,20 +1,23 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
-import { googleSignIn } from "../../../redux/features/authThunk";
 import { motion } from "framer-motion";
+import { googleSignIn } from "../../../redux/features/bookmarkThunk";
+import { setUser } from "../../../redux/features/bookmarkSlice";
 import { usePathname } from "next/navigation";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../../firebase.config";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import Image from 'next/image'
 
 const Header = () => {
   const pathname = usePathname();
   const dispatch = useAppDispatch();
   const user: any = useAppSelector((state) => state.bookmark.user);
-  console.log("user:", user)
   const [firebaseError, setFirebaseError] = useState<string>("");
 
-    const handleGoogleSignIn = async () => {
+  const handleGoogleSignIn = async () => {
     try {
       await dispatch(googleSignIn());
     } catch (error: any) {
@@ -22,17 +25,41 @@ const Header = () => {
     }
   };
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser: any) => {
+      dispatch(setUser(currentUser));
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch]);
 
   return (
     <div className='flex px-4 py-4 items-center bg-slate-900 justify-between'>
       {firebaseError && (
-          <h3 className='text-red-600 bg-red-100 px-2 py-2 rounded-md text-sm w-[20em]'>
-            {firebaseError}
-          </h3>
-         )}
+        <h3 className='text-red-600 bg-red-100 px-2 py-2 rounded-md text-sm w-[20em]'>
+          {firebaseError}
+        </h3>
+      )}
       <span className='text-sm text-orange-600'>myBookmarks</span>
-      <div className="flex gap-3">
-        <button className="px-3 py-2 bg-blue-600 text-sm text-white rounded-full" onClick={handleGoogleSignIn}>{user ? "Sign out" : "Sign in"}</button>
+      <div className='flex gap-3'>
+        <button
+          className='px-3 py-2 bg-blue-600 text-sm text-white rounded-full'
+          onClick={handleGoogleSignIn}
+        >
+          {user ? "Sign out" : "Sign in"}
+        </button>
+        {user && (
+          <Image
+            src={user?.photoURL ? user?.photoURL : ""}
+            alt={user?.email ? user?.email : ""}
+            width={500}
+            height={500}
+            className='w-[30px] h-[30px] rounded-full text-white via-cyan-900 to-stone-500 bg-gradient-to-r max-sm:cursor-pointer'
+            data-cy='user-profile-image'
+            priority
+          />
+        )}
         <button
           className={`${
             pathname === "/bookmarked"
